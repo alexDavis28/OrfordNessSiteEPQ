5c3a0242cf5842faeb2f4bbaaccfb5b1e2067267
Alex Davis <8m3xlt@gmail.com>
2022-03-22 15:14:13 +0000

    Menu bar and inheritance

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,6 @@ from flask import render_template
 
 # Home page
 @app.route('/')
-@app.route("/index")
-def hello_world():
-    return render_template("index.html")
+@app.route("/home")
+def home():
+    return render_template("home.html")
